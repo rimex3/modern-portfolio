3eafc593cdf84ab5abe54f9ff2c4e1a7f27ae90a
rimex3 <faresahmed51857@gmail.com>
2025-03-31 14:29:26 +0200

    feat: add Query Light project to the projects list

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -5,4 +5,5 @@ export const projects: ProjectCardProps[] = [
     { id: 1, name: "Ship Ninja", link: "http://shipninja.io/", imgSrc: "/imgs/ship-ninja.webp" },
     { id: 2, name: "Star SMS", link: "https://starsms.io/", imgSrc: "/imgs/starsms.webp" },
     { id: 3, name: "Swp", link: "https://swp.gg/", imgSrc: "/imgs/swp.webp" },
+    { id: 4, name: "Query Light", link: "https://github.com/rime-ui/query-light/", imgSrc: "/imgs/query-light.webp" },
 ] 
